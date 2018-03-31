5248b6c93a5b5a5fadeb704fbad2eb601ecedf0e
STcardigan <p.pondst@gmail.com>
2018-03-31 20:07:44 +0700

    delete level sys

--- Assets/ManageGame.cs
+++ Assets/ManageGame.cs
@@ -15,24 +15,13 @@ public class ManageGame : MonoBehaviour
     Sprite[] currentHaveItem  = new Sprite[53];
     int[] chkItemList = new int[53];
     int currentIndex=0 , curentNumberItem=7;
-    int itemNum = 53; //set
 
     int level = 1;
 
-    public int reqLevel2;
-    public int reqLevel3;
-    public int reqLevel4;
-    public int reqLevel5;
-    public int reqLevel6;
-    //public int reqLevel7;
-    //public int reqLevel8;
-    //public int reqLevel9;
 
-    int requireExp;
     int exp;
 
     public Text level_text;
-    public Text exp_text;
 
     // Use this for initalization
     Image image1,image2, listImgItem;
@@ -49,21 +38,16 @@ public class ManageGame : MonoBehaviour
         }
         currentHaveItem[6] = listAllImgItem[6];
         exp = curentNumberItem;
-        requireExp = reqLevel2;
-
-        level_text.text = "Level " + level;
-        exp_text.text = "Next Level : " + exp + " / " + requireExp;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        level_text.text = "Level " + level;
     }
 
     public void upDateList()
     {
-        
         int count = 0;
         for (int i = currentIndex; i < currentIndex+6; i++)
         {
@@ -97,6 +81,7 @@ public class ManageGame : MonoBehaviour
             statusItem2 = false;
         }
     }
+
     int combineItem1, combineItem2;
     public void insertItem(int i)
     {
@@ -112,12 +97,12 @@ public class ManageGame : MonoBehaviour
             combineItem2 = i;
             statusItem2 = true;
             combineItem();
+           
         }
     }
     public void combineItem()
     {
         
-
         int swip;
         if (combineItem2 < combineItem1)
         {
@@ -125,29 +110,27 @@ public class ManageGame : MonoBehaviour
             combineItem2 = combineItem1;
             combineItem1 = swip;
         }
-
+        for (int u = 0; u < 15; u++)
+        {
+            Debug.Log(u + " " + chkItemList[u] + "\n");
+        }
         formula(2, 4, 7);
         formula(0, 1, 8);
         formula(1, 8, 9);
         formula(7, 7, 10);
         formula(0, 2, 11);
         formula(0, 11, 14);
-        formula(9, 14, 26); // 
-        formula(10,26, 13);
-
-        formula(2, 2, 12);
 
     }
     public void formula(int x,int y,int z)
     {
+        Debug.Log("pppp");
         if (chkItemList[z] == 0)
         {
             if (combineItem1 == x && combineItem2 == y)
             {
                 currentHaveItem[curentNumberItem++] = listAllImgItem[z];
-                chkItemList[z] = 1;
-                exp = curentNumberItem;
-                ChangeLevel();
+                chkItemList[z] = 1;               
             }
         }
     }
@@ -159,32 +142,8 @@ public class ManageGame : MonoBehaviour
 
     void ChangeLevel()
     {
-        if (exp >= reqLevel2 && level == 1)
-        {
-            requireExp = reqLevel3;
-            level = 2;
-            //Debug.Log("level up" + level);
-        }
-        else if (exp >= reqLevel3 && level == 2)
-        {
-            requireExp = reqLevel4;
-            level = 3;
-            //Debug.Log("level up" + level);
-        }
-        else if (exp >= reqLevel4 && level == 3)
-        {
-            requireExp = reqLevel5;
-            level = 4;
-            Debug.Log("level up" + level);
-        }
-        else if (exp >= reqLevel5 && level == 4)
-        {
-            //requireExp = reqLevel6;
-            level = 5;
-            //Debug.Log("level up" + level);
-        }
+        
         level_text.text = "Level " + level;
-        exp_text.text = "Next Level : " + exp + " / " + requireExp;
 
     }
 }
